d208cf2a9f0b9dc69278d1c002e90ba3ffedc785
Piotr Pidlypczak <peterpid@gmail.com>
2018-03-10 20:21:20 +0100

    Remove empty lines

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 function getTriangleArea (a, h) {
 	if (a <= 0 || h <= 0) {
 		return 'Invalid data';
@@ -14,3 +12,4 @@ var triangleArea3 = getTriangleArea(-10, -6);
 var triangleArea4 = getTriangleArea(0,-56);
 var triangleArea5 = getTriangleArea(0, 0);
 console.log('results: '+ triangleArea1 + ', ' + triangleArea2 + ', ' + triangleArea3 + ', ' + triangleArea4 +', ' + triangleArea5);
+
