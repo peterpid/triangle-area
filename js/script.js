abb9c473ee8172f82b40d4da08a71967616aa449
Piotr Pidlypczak <peterpid@gmail.com>
2018-03-10 20:15:02 +0100

    Add function getTriangleArea

--- js/script.js
+++ js/script.js
@@ -1,38 +1,16 @@
-/*
-  Triangle Area
- 
-  added comments for task 8.4
-*/
-
-// comment below is on purpose for task 8.4
-//	alert("Invalid format of triangle base. Base must be a number.");
 
 
-function calcTriangleArea (a, h) {
+function getTriangleArea (a, h) {
+	if (a <= 0 || h <= 0) {
+		return 'Invalid data';
+	}
 	return a * h / 2;
 }
 
-var triangleBase = prompt("Please enter triangle base");
-if (triangleBase !== null) {
-	console.log('triangleBase: ' + triangleBase);
-	var a = Number(triangleBase); 	
-	if (!isNaN(a) && a >= 0) {
-		var triangleHeight = prompt('Please enter triangle height');
-		if (triangleHeight !== null) {
-			var h = Number(triangleHeight); 	
-			if (!isNaN(h) && h >= 0) {
-				var triangleArea = calcTriangleArea(a, h);
-				console.log('Triangle area for base a: ' + a + ' and height h: ' + h + ' is: ' + triangleArea);
-				alert('Triangle area is: ' + triangleArea);
-			} else {
-				alert('Invalid format of triangle height. Height must be a number and > 0.');
-			}
-		} else {
-			alert('Invalid format of triangle height. Height must be a number.');
-		}
-	} else {
-		alert('Invalid format of triangle base. Base must be a number and > 0.');
-	}
-} else {
-	alert('Invalid format of triangle base. Base must be a number.');
-}
+console.log(getTriangleArea(10 ,6));
+var triangleArea1 = getTriangleArea(10, 6);
+var triangleArea2 = getTriangleArea(24.3, 5.3);
+var triangleArea3 = getTriangleArea(-10, -6);
+var triangleArea4 = getTriangleArea(0,-56);
+var triangleArea5 = getTriangleArea(0, 0);
+console.log('results: '+ triangleArea1 + ', ' + triangleArea2 + ', ' + triangleArea3 + ', ' + triangleArea4 +', ' + triangleArea5);
